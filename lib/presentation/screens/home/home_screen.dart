71e2f07e05a2ec4ed08664c012958d95bcb2dc7a
juanpahv <dcrunneryt1@gmail.com>
2024-11-21 22:42:54 +0900

    add home_screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Inicio',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.icon(
+              onPressed: () {
+                context.push("/create_profile");
+              },
+              label: const Text('Crear Perfil', style: TextStyle(fontSize: 18)),
+              icon: const Icon(Icons.person_add_rounded, size: 28),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
